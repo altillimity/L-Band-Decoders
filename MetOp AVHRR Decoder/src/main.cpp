a106fe919bd41c24524c47b68c0d09b2ea92d314
Aang23 <qwerty15@gmx.fr>
2020-07-26 17:20:49 +0200

    MetOp AVHRR Decoder comments

--- MetOp AVHRR Decoder/src/main.cpp
+++ MetOp AVHRR Decoder/src/main.cpp
@@ -28,15 +28,13 @@ int main(int argc, char *argv[])
     // AVHRR Packet buffer
     uint16_t avhrrBuffer[12944];
 
-    int avhrrPacket = 0;
-    int bitsInBuf = 0;
-    uint16_t buf = 0;
-
+    // Frame counter
     int frame = 0;
 
-    unsigned short *imageBufferR = new unsigned short[2000 * 2048];
-    unsigned short *imageBufferG = new unsigned short[2000 * 2048];
-    unsigned short *imageBufferB = new unsigned short[2000 * 2048];
+    // This will need some fixes
+    unsigned short *imageBufferR = new unsigned short[10000 * 2048];
+    unsigned short *imageBufferG = new unsigned short[10000 * 2048];
+    unsigned short *imageBufferB = new unsigned short[10000 * 2048];
 
     // Read until EOF
     while (!data_in.eof())
@@ -44,13 +42,13 @@ int main(int argc, char *argv[])
         // Read buffer
         data_in.read((char *)buffer, BUFFER_SIZE);
 
+        // Print APID
         int apid = ((buffer[0] % ((int)pow(2, 3))) << 8) | buffer[1];
+        std::cout << "APID : " << apid << std::endl;
 
-        std::cout << apid << std::endl;
+        int pos = 20; // AVHRR Data, eg, User Data Field
 
-        avhrrPacket = 0;
-
-        int pos = 20;
+        // Convert into 10-bits values
         for (int i = 0; i < 12944; i += 4)
         {
             avhrrBuffer[i] = (buffer[pos + 0] << 2) | (buffer[pos + 1] >> 6);
@@ -60,27 +58,32 @@ int main(int argc, char *argv[])
             pos += 5;
         }
 
+        // Channel R
         for (int i = 0; i < 2048; i++)
         {
             uint16_t pixel = avhrrBuffer[55 + 1 + i * 5];
             imageBufferR[frame * 2048 + i] = pixel * 60;
         }
 
+        // Channel G
         for (int i = 0; i < 2048; i++)
         {
             uint16_t pixel = avhrrBuffer[55 + 1 + i * 5];
             imageBufferG[frame * 2048 + i] = pixel * 60;
         }
 
+        // Channel B
         for (int i = 0; i < 2048; i++)
         {
             uint16_t pixel = avhrrBuffer[55 + 0 + i * 5];
             imageBufferB[frame * 2048 + i] = pixel * 60;
         }
 
+        // Frame counter
         frame++;
     }
 
+    // Print it all out into a .png
     cimg_library::CImg<unsigned short> finalImage(2048, frame, 1, 3);
 
     cimg_library::CImg<unsigned short> channelImageR(&imageBufferR[0], 2048, frame);
