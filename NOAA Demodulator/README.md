4e3a784eccfd4c0ae233be4ff2248ac78db904e2
Aang23 <qwerty15@gmx.fr>
2020-09-05 00:26:49 +0200

    NOAA Demod : this will need a cleanup

---
+++ NOAA Demodulator/README.md
@@ -0,0 +1,3 @@
+# NOAA HRPT Demodulator
+
+Simple standalone NOAA HRPT demodulator and deframer. See --help for syntax.
--- NOAA Demodulator/src/main.cpp
+++ NOAA Demodulator/src/main.cpp
@@ -3,13 +3,12 @@
 #include <complex>
 #include <vector>
 #include <cstdint>
-
+#include "tclap/CmdLine.h"
 #include "agc.h"
 #include "pll.h"
 #include "moving_average.h"
 #include "clock_recovery.h"
 #include "deframer.h"
-#include "pp_buffer.h"
 
 // Return filesize
 size_t getFilesize(std::string filepath)
@@ -40,17 +39,66 @@ void volk_32f_binary_slicer_8i_generic(int8_t *cVector, const float *aVector, un
 }
 
 #define BUFFER_SIZE 8192 * 10
-#define SAMPLE_RATE (float)6e6
 #define SYMBOL_RATE (float)(600 * 1109)
-#define SPS (SAMPLE_RATE / SYMBOL_RATE)
+
+/* TODO : THIS WILL NEED A HUGE CLEANUP */
 
 int main(int argc, char *argv[])
 {
-    std::ifstream data_in(argv[1], std::ios::binary);
-    std::ofstream data_out(argv[2], std::ios::binary);
+    TCLAP::CmdLine cmd("NOAA HRPT Demodulator / Deframer by Aang23", ' ', "1.0");
 
-    size_t filesize = getFilesize(argv[1]);
+    // File arguments
+    TCLAP::ValueArg<std::string> valueInput("i", "input", "Baseband input", true, "", "baseband.raw");
+    TCLAP::ValueArg<std::string> valueOutput("o", "output", "Output frames", true, "", "frames.bin");
 
+    // VCID / APID to extract
+    TCLAP::ValueArg<int> valueSamplerate("s", "samplerate", "Baseband samplerate", true, 6000000, "6000000");
+
+    // Baseband format
+    TCLAP::SwitchArg valueF32Baseband("f", "float32", "Input baseband as float32");
+    TCLAP::SwitchArg valueInt16Baseband("6", "int16", "Input baseband as int16");
+    TCLAP::SwitchArg valueInt8Baseband("8", "int8", "Input baseband as int8");
+
+    // Register all of the above options
+    cmd.add(valueInput);
+    cmd.add(valueOutput);
+    cmd.add(valueSamplerate);
+    cmd.add(valueF32Baseband);
+    cmd.add(valueInt16Baseband);
+    cmd.add(valueInt8Baseband);
+
+    // Parse
+    try
+    {
+        cmd.parse(argc, argv);
+    }
+    catch (TCLAP::ArgException &e)
+    {
+        std::cout << e.error() << '\n';
+        return 0;
+    }
+
+    // Just in case
+    if (!(valueF32Baseband.getValue() || valueInt16Baseband.getValue() || valueInt8Baseband.getValue()))
+    {
+        std::cout << "Please specify baseband format! -f, -6, -8" << std::endl;
+        return 1;
+    }
+    else if ((valueF32Baseband.getValue() + valueInt16Baseband.getValue() + valueInt8Baseband.getValue()) > 1)
+    {
+        std::cout << "Please specify only one baseband format! -f, -6, -8" << std::endl;
+        return 1;
+    }
+
+    // Filezise and progress stuff
+    size_t filesize = getFilesize(valueInput.getValue());
+    size_t dataout = 0;
+
+    // Input / output files
+    std::ifstream data_in(valueInput.getValue(), std::ios::binary);
+    std::ofstream data_out(valueOutput.getValue(), std::ios::binary);
+
+    // Buffer mess
     std::complex<float> buffer[BUFFER_SIZE];
     std::complex<float> agc_buff[BUFFER_SIZE];
     float pll_buff[BUFFER_SIZE];
@@ -61,23 +109,56 @@ int main(int argc, char *argv[])
     std::vector<std::complex<float>> clockRecoOut;
     std::vector<uint16_t> frames;
 
-    int framec = 0;
+    int frame_count = 0;
 
     Agc agc = Agc(AGC_WINSIZE, 0.5 / 32768.0, 1.0);
-    PLL pll = PLL(0.01f, pow(0.01, 2) / 4.0, 3.0f * M_PI * 100e3f / SAMPLE_RATE);
-    MovingAverage movingAverage = MovingAverage(round(SPS / 4.0f), 1.0f / 4.0, BUFFER_SIZE, 1);
-    ClockRecovery clockReco = ClockRecovery(SPS / 2.0f, pow(0.01, 2) / 4.0, 0.5f, 0.01f, 100e-6f);
+    PLL pll = PLL(0.01f, pow(0.01, 2) / 4.0, 3.0f * M_PI * 100e3f / (float) valueSamplerate.getValue());
+    MovingAverage movingAverage = MovingAverage(round(((float) valueSamplerate.getValue() / SYMBOL_RATE) / 4.0f), 1.0f / 4.0, BUFFER_SIZE, 1);
+    ClockRecovery clockReco = ClockRecovery(((float) valueSamplerate.getValue() / SYMBOL_RATE) / 2.0f, pow(0.01, 2) / 4.0, 0.5f, 0.01f, 100e-6f);
     NOAADeframer deframer;
+
+    // Int16 buffer
+    int16_t buffer_i16[BUFFER_SIZE * 2];
+
+    // Int8 buffer
+    int8_t buffer_i8[BUFFER_SIZE * 2];
 
     while (!data_in.eof())
     {
-        data_in.read((char *)buffer, BUFFER_SIZE * sizeof(std::complex<float>));
+        // Get baseband, possibly convert to F32
+        if (valueF32Baseband.getValue())
+        {
+            data_in.read((char *)buffer, BUFFER_SIZE * sizeof(std::complex<float>));
+        }
+        else if (valueInt16Baseband.getValue())
+        {
+            data_in.read((char *)buffer_i16, BUFFER_SIZE * sizeof(int16_t) * 2);
+            for (int i = 0; i < BUFFER_SIZE; i++)
+            {
+                using namespace std::complex_literals;
+                buffer[i] = (float)buffer_i16[i * 2] + (float)buffer_i16[i * 2 + 1] * 1if;
+            }
+        }
+        else if (valueInt8Baseband.getValue())
+        {
+            data_in.read((char *)buffer_i8, BUFFER_SIZE * sizeof(int8_t) * 2);
+            for (int i = 0; i < BUFFER_SIZE; i++)
+            {
+                using namespace std::complex_literals;
+                buffer[i] = (float)buffer_i8[i * 2] + (float)buffer_i8[i * 2 + 1] * 1if;
+            }
+        }
 
+        // Agc
         agc.work(buffer, BUFFER_SIZE, agc_buff);
+
+        // Carrier-tracking PLL
         pll.work(agc_buff, BUFFER_SIZE, pll_buff);
 
+        // Average
         int numMovingAvg = movingAverage.work(pll_buff, BUFFER_SIZE, moving_buff);
 
+        // This is pure junk and slow as heck, but it got the thing working for now
         for (int i = 0; i < numMovingAvg; i++)
             clockRecoIn.push_back(std::complex<float>(moving_buff[i], 0));
         clockRecoOut = clockReco.work(clockRecoIn, clockRecoIn.size());
@@ -85,21 +166,25 @@ int main(int argc, char *argv[])
         for (int i = 0; i < recovered_size; i++)
             recovered_buff[i] = clockRecoOut[i].real();
 
+        // Deframe
         if (recovered_size > 0)
         {
             volk_32f_binary_slicer_8i_generic((int8_t *)bitsBuffer, recovered_buff, recovered_size);
             frames = deframer.work(bitsBuffer, recovered_size);
         }
 
-        framec += frames.size();
+        // Count frames
+        frame_count += frames.size();
 
+        // Write to file
         if (frames.size() > 0)
             for (uint16_t &frame : frames)
                 data_out.write((char *)&frame, sizeof(uint16_t));
 
         // Console stuff
-        std::cout << '\r' << "Frames : " << framec / 11090 << ", Data out : " << round(framec * 2 / 1e5) / 10.0f << " MB, Progress : " << round(((float)data_in.tellg() / (float)filesize) * 1000.0f) / 10.0f << "%     " << std::flush;
+        std::cout << '\r' << "Frames : " << frame_count / 11090 << ", Data out : " << round(frame_count * 2 / 1e5) / 10.0f << " MB, Progress : " << round(((float)data_in.tellg() / (float)filesize) * 1000.0f) / 10.0f << "%     " << std::flush;
 
+        // Clear vectors
         frames.clear();
         clockRecoOut.clear();
         clockRecoIn.clear();
