59ee5f1ddd33fb4f4ae8e80ed7f9af756ac83b28
ZbychuButItWasTaken <sztanga.zbyszek@gmail.com>
2021-01-03 22:25:08 +0100

    fixed NOAA MHS Decoder

--- NOAA MHS Decoder/src/main.cpp
+++ NOAA MHS Decoder/src/main.cpp
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
             for (int i = 1; i < 6; i++)
             {
                 //get the MIU minor cycle count
-                uint16_t MHSnum = buffer[104 * i + 6];
+                unsigned short MHSnum = buffer[104 * i + 6];
                 MHSnum >>= 2;
 
                 //
@@ -67,30 +67,31 @@ int main(int argc, char *argv[])
                 //read the MHS Data from AIP frames (one of 3 in each AIP frame)
                 if (MHSnum == 27)
                 {
+                    std::fill_n(MHSWord, 643, 0);
                     for (int j = 0; j < 18; j += 2)
                     {
-                        MHSWord[j / 2] = (buffer[104 * i + 79 + j] % (int)pow(2, 8)) << 8 | (buffer[104 * i + 78 + j] % (int)pow(2, 8));
+                        MHSWord[j / 2] = (buffer[104 * i + 80 + j] >> 2) << 8 | (buffer[104 * i + 79 + j]>> 2);
                     }
                 }
                 else if (MHSnum > 27 && MHSnum < 53)
                 {
                     for (int j = 0; j < 50; j += 2)
                     {
-                        MHSWord[j / 2 + 1 + 25 * MHSnum] = (buffer[104 * i + 47 + j] % (int)pow(2, 8)) << 8 | (buffer[104 * i + 46 + j] % (int)pow(2, 8));
+                        MHSWord[j / 2 + 1 + 25 * (MHSnum - 28)] = (buffer[104 * i + 48 + j]>> 2) << 8 | (buffer[104 * i + 47 + j]>> 2);
                     }
                 }
                 else if (MHSnum == 53)
                 {
                     for (int j = 0; j < 18; j += 2)
                     {
-                        MHSWord[642 + j / 2] = (buffer[104 * i + 47 + j] % (int)pow(2, 8)) << 8 | (buffer[104 * i + 46 + j] % (int)pow(2, 8));
+                        MHSWord[633 + j / 2] = (buffer[104 * i + 48 + j]>> 2) << 8 | (buffer[104 * i + 47 + j]>> 2);
                     }
 
                     for (int j = 0; j < 540; j += 6)
                     {
                         //idk why I needed to add +49 there, but doesn't work without it
                         for(int k = 0; k<5; k++)
-                            channels[k][line * 90 + 90 - j / 6] = MHSWord[j + k + 50];
+                            channels[k][line * 90 + 90 - j / 6 - 1] = MHSWord[j + k + 17];
                     }
                     line = line + 1;
                 }
@@ -106,16 +107,17 @@ int main(int argc, char *argv[])
                 //read the MHS Data from AIP frames (one of 3 in each AIP frame)
                 if (MHSnum == 54)
                 {
+                    std::fill_n(MHSWord, 643, 0);
                     for (int j = 0; j < 36; j += 2)
                     {
-                        MHSWord[j / 2] = (buffer[104 * i + 62 + j] % (int)pow(2, 8)) << 8 | (buffer[104 * i + 61 + j] % (int)pow(2, 8));
+                        MHSWord[j / 2] = (buffer[104 * i + 62 + j]>> 2) << 8 | (buffer[104 * i + 61 + j]>> 2);
                     }
                 }
-                else if (MHSnum > 36 && MHSnum < 80)
+                else if (MHSnum > 54 && MHSnum < 80)
                 {
                     for (int j = 0; j < 50; j += 2)
                     {
-                        MHSWord[j / 2 + 1 + 25 * MHSnum] = (buffer[104 * i + 48 + j] % (int)pow(2, 8)) << 8 | (buffer[104 * i + 47 + j] % (int)pow(2, 8));
+                        MHSWord[j / 2 + 1 + 25 * (MHSnum-55)] = (buffer[104 * i + 48 + j]>> 2) << 8 | (buffer[104 * i + 47 + j]>> 2);
                     }
                 }
                 if (MHSnum == 79)
@@ -125,7 +127,7 @@ int main(int argc, char *argv[])
                     {
                         //idk why I needed to add +49 there, but doesn't work without it
                         for (int k = 0; k < 5; k++)
-                            channels[k][line * 90 + 90 - j / 6] = MHSWord[j + k + 50];
+                            channels[k][line * 90 + 90 - j / 6 - 1] = MHSWord[j + k + 8];
                     }
 
                     line = line + 1;
@@ -138,32 +140,32 @@ int main(int argc, char *argv[])
                 //
                 //first packet start (PKT 2)
                 //
+                
 
                 //read the MHS Data from AIP frames (one of 3 in each AIP frame)
                 if (MHSnum == 0)
                 {
                     std::fill_n(MHSWord, 643, 0);
-                    MHSWord[0] = (buffer[104 * i + 96] % (int)pow(2, 8)) << 8 | (buffer[104 * i + 95] % (int)pow(2, 8));
+                    MHSWord[0] = (buffer[104 * i + 96]>> 2) << 8 | (buffer[104 * i + 95]>> 2);
                 }
                 else if (MHSnum > 0 && MHSnum < 26)
                 {
                     for (int j = 0; j < 50; j += 2)
                     {
-                        MHSWord[j / 2 + 1 + 25 * MHSnum] = (buffer[104 * i + 48 + j] % (int)pow(2, 8)) << 8 | (buffer[104 * i + 47 + j] % (int)pow(2, 8));
+                        MHSWord[j / 2 + 1 + 25 * (MHSnum - 1)] = (buffer[104 * i + 48 + j]>> 2) << 8 | (buffer[104 * i + 47 + j]>> 2);
                     }
                 }
                 else if (MHSnum == 26)
                 {
                     for (int j = 0; j < 34; j += 2)
                     {
-                        MHSWord[642 + j / 2] = (buffer[104 * i + 48 + j] % (int)pow(2, 8)) << 8 | (buffer[104 * i + 47 + j] % (int)pow(2, 8));
+                        MHSWord[625 + j / 2] = (buffer[104 * i + 48 + j]>> 2) << 8 | (buffer[104 * i + 47 + j]>> 2);
                     }
 
-                    for (int j = 0; j < 540; j += 6)
+                    for (int j = 0; j < 90; j += 1)
                     {
-                        //idk why I needed to add +49 there, but doesn't work without it
                         for (int k = 0; k < 5; k++)
-                            channels[k][line * 90 + 90 - j / 6] = MHSWord[j + k + 50];
+                            channels[k][line * 90 + 90 - j - 1] = MHSWord[j*6 + k + 25];
                     }
 
                     line = line + 1;
@@ -194,12 +196,13 @@ int main(int argc, char *argv[])
     cimg_library::CImg<unsigned short> imgAll(270, (line + 1) * 2);
     imgAll.fill(0);
     
-
+    
     channel1.equalize(1000);
     channel2.equalize(1000);
     channel3.equalize(1000);
     channel4.equalize(1000);
     channel5.equalize(1000);
+    
     
 
     imgAll.draw_image(0, 0, channel1);
