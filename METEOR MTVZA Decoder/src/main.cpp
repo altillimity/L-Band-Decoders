ccdc8dfcc7828b08a7f9609c8fd496f41dcf7edb
Aang23 <qwerty15@gmx.fr>
2021-01-09 23:37:57 +0100

    Finish the METEOR MTVZA Decoder

--- METEOR MTVZA Decoder/src/main.cpp
+++ METEOR MTVZA Decoder/src/main.cpp
@@ -3,106 +3,89 @@
 #include <complex>
 #include <vector>
 
-#define cimg_use_png
-#define cimg_display 0
-#include "CImg.h"
+#include "mtvza_reader.h"
 
-// Processing buffer size
-#define BUFFER_SIZE (248 * 2)
-
-// Returns the asked bit!
-template <typename T>
-inline bool getBit(T &data, int &bit)
+// Return filesize
+size_t getFilesize(std::string filepath)
 {
-    return (data >> bit) & 1;
+    std::ifstream file(filepath, std::ios::binary | std::ios::ate);
+    std::size_t fileSize = file.tellg();
+    file.close();
+    return fileSize;
 }
+
+// IO files
+std::ifstream data_in;
 
 int main(int argc, char *argv[])
 {
-    // Output and Input file
-    std::ifstream data_in(argv[1], std::ios::binary);
+    if (argc != 2)
+    {
+        std::cout << "Usage: " << argv[0] << " inputFrames.bin" << std::endl;
+        return 0;
+    }
 
     // Read buffer
-    uint8_t buffer[BUFFER_SIZE];
+    uint8_t buffer[248];
 
-    // AVHRR Packet buffer
-    uint16_t msumrBuffer[120];
+    // Complete filesize
+    size_t filesize = getFilesize(argv[1]);
 
-    uint16_t mtvza_buffer[21][120];
+    // Output and Input file
+    data_in = std::ifstream(argv[1], std::ios::binary);
 
-    // Frame counter
-    int frame = 0;
+    // Graphics
+    std::cout << "---------------------------" << std::endl;
+    std::cout << "   METEOR MTVZA Decoder" << std::endl;
+    std::cout << "         by Aang23" << std::endl;
+    std::cout << "---------------------------" << std::endl;
+    std::cout << std::endl;
 
-    // This will need some fixes
-    unsigned short *imageBufferR = new unsigned short[10000 * 120];
-    unsigned short *imageBufferG = new unsigned short[10000 * 200];
-    unsigned short *imageBufferB = new unsigned short[10000 * 200];
+    std::cout << "Processing..." << std::endl;
 
-    bool done = false;
+    MTVZAReader reader;
 
     // Read until EOF
     while (!data_in.eof())
     {
-        //while (!done && !data_in.eof())
-        //{
         // Read buffer
-        data_in.read((char *)buffer, BUFFER_SIZE);
+        data_in.read((char *)buffer, 248);
 
-        if (buffer[5] != std::stoi(argv[3]))
-            continue;
+        reader.work(buffer);
 
-        for (int l = 0; l < 120; l += 4)
-        {
-            int pixelpos = 3 + l;
-
-            msumrBuffer[buffer[5] * 2 + 0] = buffer[pixelpos + 0] << 1 | buffer[pixelpos + 1];
-            msumrBuffer[buffer[5] * 2 + 1] = buffer[pixelpos + 2] << 1 | buffer[pixelpos + 3];
-            msumrBuffer[l + 2] = buffer[pixelpos * 2 + 4] << 1 | buffer[pixelpos * 2 + 5];
-            msumrBuffer[l + 3] = buffer[pixelpos * 2 + 6] << 1 | buffer[pixelpos * 2 + 7];
-        }
-
-        //if (buffer[5] > 25)
-        //   done = true;
-        //}
-
-        // Channel R
-        for (int i = 0; i < 120; i++)
-        {
-            uint16_t pixel = msumrBuffer[i];
-            imageBufferR[frame * 120 + i] = pixel * 100;
-        }
-
-        // Channel G
-        /*for (int i = 0; i < 10; i++)
-        {
-            uint16_t pixel = msumrBuffer[i];
-            imageBufferG[frame * 200 + i] = pixel * 100;
-        }
-
-        // Channel B
-        for (int i = 0; i < 10; i++)
-        {
-            uint16_t pixel = msumrBuffer[i];
-            imageBufferB[frame * 200 + i] = pixel * 100;
-        }
-        */
-        // Frame counter
-        frame++;
-        //done = false;
+        // Show our progress
+        std::cout << "\rProgress : " << round(((float)data_in.tellg() / (float)filesize) * 1000.0f) / 10.0f << "%     " << std::flush;
     }
 
-    // Print it all out into a .png
-    cimg_library::CImg<unsigned short> finalImage(200, frame, 1, 3);
+    std::cout << std::endl;
+    std::cout << std::endl;
 
-    cimg_library::CImg<unsigned short> channelImageR(&imageBufferR[0], 120, frame);
-    cimg_library::CImg<unsigned short> channelImageG(&imageBufferG[0], 200, frame);
-    cimg_library::CImg<unsigned short> channelImageB(&imageBufferB[0], 200, frame);
+    std::cout << "MTVZA Lines            : " << reader.lines << std::endl;
 
-    finalImage.draw_image(0, 0, 0, 0, channelImageR);
-    finalImage.draw_image(0, 0, 0, 1, channelImageG);
-    finalImage.draw_image(0, 0, 0, 2, channelImageB);
+    std::cout << std::endl;
 
-    channelImageR.save_png(argv[2]);
+    // Write images out
+    std::cout << "Writing images... (Can take a while)" << std::endl;
+
+    std::cout << "Global Composite..." << std::endl;
+    int all_width_count = 20;
+    int all_height_count = 6;
+    cimg_library::CImg<unsigned short> imageAll(26 * 5 * all_width_count, reader.getChannel(0).height() * all_height_count, 1, 1);
+    {
+        int height = reader.getChannel(0).height();
+
+        for (int row = 0; row < all_height_count; row++)
+        {
+            for (int column = 0; column < all_width_count; column++)
+            {
+                if (row * all_width_count + column >= 8461)
+                    break;
+
+                imageAll.draw_image(26 * 5 * column, height * row, 0, 0, reader.getChannel(row * all_width_count + column));
+            }
+        }
+    }
+    imageAll.save_png("MTVZA-ALL.png");
 
     data_in.close();
 }
